189327a95e16c873ac9fec1ec60fa47021b58964
UNAINR <munain605@gmail.com>
2025-06-05 23:47:58 +0500

    first-commit

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
       }
     ]
   },
+   eslint: {
+       ignoreDuringBuilds: true, // Ignore ESLint errors during builds
+     },
 };
 
 export default nextConfig;
